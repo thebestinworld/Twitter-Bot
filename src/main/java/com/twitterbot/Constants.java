039be602d3caf6d34406c4b2ba8ebea94e367c36
Jose David <spambg97@gmail.com>
2018-10-26 22:12:11 +0300

    Update Constants.java

--- src/main/java/com/twitterbot/Constants.java
+++ src/main/java/com/twitterbot/Constants.java
@@ -2,8 +2,8 @@ package com.twitterbot;
 
 public class Constants {
 
-    public final static String CONSUMER_KEY ="PDlcVV9MLWGC2P6U95G3Qv1QD";
-    public final static String CONSUMER_SECRET="GYG9ErE2QHVQM3dES6RQvXcdyvUbllKMA7iKPTFvgUYLHqosTr";
-    public final static String ACCESS_TOKEN="1944271992-ZOgTPqOanhR2H7gJfvkD3dO8qITbNoOouK1R592";
-    public final static String TOKEN_SECRET="F8kd2eRTydAIP6JvEJsDyGu10zFSHdMxoSLjfw2Wq8F0Z";
+    public final static String CONSUMER_KEY ="";
+    public final static String CONSUMER_SECRET="";
+    public final static String ACCESS_TOKEN="";
+    public final static String TOKEN_SECRET="";
 }
